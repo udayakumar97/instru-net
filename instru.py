f8ca09a273c6256b8cc2e3dd75ff55ab37114de3
Udaya Kumar <udayakumar97@gmail.com>
2018-07-11 21:45:35 +0530

    Debugging

--- instru.py
+++ instru.py
@@ -73,7 +73,7 @@ print(imgs_mask_train.shape)
 print('Loading done')
 model_checkpoint = ModelCheckpoint('instru.hdf5', monitor='loss',verbose=1, save_best_only=True)
 
-instru.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=1, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+instru.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
 imgs_test=imgs_train[:30]
 imgs_mask_test = instru.predict(imgs_test, batch_size=1, verbose=1)
 #print(imgs_test)
