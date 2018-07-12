f2f5345c439a1098969663ae50b61fe4aa747219
Udaya Kumar <udayakumar97@gmail.com>
2018-07-13 00:40:41 +0530

    Changed epoch to 3

--- instru.py
+++ instru.py
@@ -75,7 +75,7 @@ print(imgs_mask_train.shape)
 print('Loading done')
 model_checkpoint = ModelCheckpoint('../drive/checkpoints/instru.hdf5', monitor='loss',verbose=1, save_best_only=True)
 instru.load_weights('../drive/checkpoints/instru.hdf5')
-instru.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+instru.fit(imgs_train, imgs_mask_train, batch_size=2, nb_epoch=3, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
 imgs_test=imgs_train[:30]
 imgs_mask_test = instru.predict(imgs_test, batch_size=1, verbose=1)
 #print(imgs_test)
